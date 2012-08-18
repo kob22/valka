d80b77ff8fcb11fc28c998597aad139d8b648807
kob22 <kob2222@gmail.com>
2012-08-18 14:24:52 +0200

    kolejny start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Valka::Application.config.secret_token = '6d1022a3cf77f7700ab9311f6301cde7960b49c94e335a25e27ef59c70c036b23c2b09d798bd9bfa535f1720be048bec0db3dd636293eaff824b7738395df351'
